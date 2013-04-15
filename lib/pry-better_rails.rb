a2810329d4035b18ca402a088dc09023a7db378e
fism88+☈king <fism88+rking@sharpsaw.org>
2013-04-14 23:20:49 -0500

    db-{quiet,loud}-log

--- lib/pry-better_rails.rb
+++ lib/pry-better_rails.rb
@@ -6,4 +6,23 @@ require 'better_errors/rails'
 BetterErrors.use_pry!
 
 module PryBetterRails
+  class DbQuietLog < Pry::ClassCommand
+    match 'db-quiet-log'
+    group 'Rails'
+    description 'Silence ActiveRecord logs'
+    def process
+      ActiveRecord::Base.logger = nil
+    end
+  end
+  Pry::Commands.add_command DbQuietLog
+
+  class DbLoudLog < Pry::ClassCommand
+    match 'db-loud-log'
+    group 'Rails'
+    description 'Verbosify ActiveRecord logs'
+    def process
+      ActiveRecord::Base.logger = Logger.new $stderr
+    end
+  end
+  Pry::Commands.add_command DbLoudLog
 end
