2ee05fc956e6cd1f120bf0120971f9d2a7158ddc
fism88+☈king <fism88+rking@sharpsaw.org>
2013-04-14 23:51:09 -0500

    Add background eager_load! (for slow projects)

--- lib/pry-better_rails.rb
+++ lib/pry-better_rails.rb
@@ -5,6 +5,31 @@ require 'better_errors/rails'
 
 BetterErrors.use_pry!
 
+Pry.hooks.add_hook :when_started, :preload_for_speedy_show_models do
+  Thread.start do
+    Rails.application.eager_load!
+    save_pos      = "\e[s"
+    goto_top_left = "\e[0;0H"
+    restore_pos   = "\e[u"
+    periwinkle    = "\e[38;5;67m"
+    brightblue    = "\e[38;5;51m"
+    color_off     = "\e[0m"
+    print [
+      save_pos, goto_top_left,
+      '[',
+      periwinkle, 'pry-better_rails finished background eager_load!, ',
+      brightblue, 'show-models',
+      periwinkle, ' now loaded',
+      color_off,
+      ']',
+      restore_pos
+    ].join
+  end
+end
+
+# TODO
+# FactoryGirl.reload
+
 module PryBetterRails
   class DbQuietLog < Pry::ClassCommand
     match 'db-quiet-log'
